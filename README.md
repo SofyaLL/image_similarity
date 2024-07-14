b409ad2b7c6750388832fad4c7e77e5bffa88dd6
SofyaLL <loskutovaxsp@gmail.com>
2024-07-14 14:09:50 +0200

    type hints in utils.py, correct notebook

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# image similarity
+# Fast Image Similarity
+
+This code allows you to calculate a similarity score between images. The algorithm can be used in projects where you need to find similar images from a collection of images. It works well on images containing the same objects, such as grocery goods or product labels.
+
+The algorithm is based on the ORB Keypoint Detector in OpenCV. For matching keypoints obtained by the detector, the Brute-Force matcher with knnMatch is used along with a ratio test to filter out good matches. By considering the number of good matches and the number of keypoints in a pair of images, it is possible to calculate a similarity score.
+
+$$Similarity\ Score = \frac{N\ matched\ keypoints}{\max(N\ keypoints_{first\ image} , N\ keypoints_{second\ image})}$$
+
+where $N$ stands for number.
+
+## Required Packages
+- python 
+- opencv
+- jupyter (optional: to run jupyter notebook)
+- matplotlib (optional: to plot results in the notebook)
+
+## File Descriptions
+- `example.ipynb` — Jupyter Notebook with examples of calculating similarity score. 
+- `utils.py` — a file with necessary functions to calculate similarity. 
+
+## Time consuption
+
+I provide results of some my experiments. With this data I want to show that ORB feature detector can be optimized in terms of consuptiong time. It can be important in real-time products. As you can see, matching time between two images rapidly decreased with number of keypoints detected by ORB. For example, if you want to compair 10 000 images pairs you can gain a significant amount of time (2.7 s for ORB detector with 150 keypoints vc 10.3 s for default ORB detector with 500 keypoints). 
+
+|                                                          | Extracting Time, ms | Average Number of Detected Keypoints per Image | Matching Time Between Two Images, ms |
+| -------------------------------------------------------- | ------------------- | ---------------------------------------------- | ------------------------------------ |
+| ORB detector nfeatures=150<br>image size = 256           | 1.36                | 135                                            | 0.27                                 |
+| ORB detector nfeatures=300<br>image size = 256           | 1.45                | 256                                            | 0.61                                 |
+| ORB detector default (nfeatures=500)<br>image size = 256 | 1.77                | 408                                            | 1.03                                 |
+| ORB detector nfeatures=600<br>image size = 256           | 1.63                | 478                                            | 1.37                                 |
+|                                                          |                     |                                                |                                      |
+| ORB detector nfeatures=150<br>image size = 256           | 1.36                | 135                                            | 0.27                                 |
+| ORB detector nfeatures=150<br>image size = 350           | 2.36                | 147                                            | 0.30                                 |
+| ORB detector nfeatures=150<br>image size = 500           | 3.91                | 150                                            | 0.32                                 |
--- utils.py
+++ utils.py
@@ -1,57 +1,38 @@
-import heapq
-import pickle
-import time
-import traceback
+from typing import List, Tuple
 
 import cv2
+import numpy as np
 
 orb = cv2.ORB_create(nfeatures=150)
 bf = cv2.BFMatcher(cv2.NORM_HAMMING)
 
 
-def load_data_from_pickle(pickle_path):
-    with open(pickle_path, "rb") as file:
-        try:
-            data = pickle.load(file)
-            return data
-        except:
-            print(traceback.format_exc())
-
-
-def save_data_to_pickle(pickle_path, data_base):
-    with open(pickle_path, "wb") as file:
-        pickle.dump(data_base, file)
-
-
-def compute_orb(image):
+def compute_orb(image: np.ndarray) -> Tuple[List[cv2.KeyPoint], np.ndarray]:
     """
     Compute ORB (Oriented FAST and Rotated BRIEF) keypoints and descriptors for a given image.
 
     Parameters:
-    image (numpy.ndarray): The input image for which to compute the ORB keypoints and descriptors.
-                           This should be a grayscale image of type numpy array.
+    image (numpy.ndarray): The input grayscale image for which to compute the ORB keypoints and descriptors.
 
     Returns:
-    tuple: A tuple containing:
-        - key_points (list): A list of detected keypoints. Each keypoint is an instance of cv2.KeyPoint.
-        - descriptors (numpy.ndarray): An array of computed descriptors corresponding to the keypoints.
-                                       Each descriptor is a vector of integers.
+    - key_points (List[cv2.KeyPoint]): A list of detected keypoints.
+    - descriptors (np.ndarray): An array of computed descriptors corresponding to the keypoints.
     """
     key_points, descriptors = orb.detectAndCompute(image, None)
-    return (key_points, descriptors)
+    return key_points, descriptors
 
 
-def calculate_matches(descriptor_1, descriptor_2):
+def calculate_matches(descriptor_1: np.ndarray, descriptor_2: np.ndarray) -> List[List[cv2.DMatch]]:
     """
     Calculate good matches between two sets of ORB descriptors using the BFMatcher with the KNN algorithm.
 
     Parameters:
-    descriptor_1 (numpy.ndarray): Descriptors of the first image.
-    descriptor_2 (numpy.ndarray): Descriptors of the second image.
+    descriptor_1 (np.ndarray): Descriptors of the first image.
+    descriptor_2 (np.ndarray): Descriptors of the second image.
 
     Returns:
-    list: A list of good matches. Each good match is a list containing a single DMatch object,
-          where a DMatch object has attributes queryIdx, trainIdx, imgIdx, and distance.
+    List[List[cv2.DMatch]]:
+        A list of good matches. Each good match is a list containing a single DMatch object.
     """
     ratio = 0.89
     matches = bf.knnMatch(descriptor_1, descriptor_2, k=2)
@@ -69,17 +50,17 @@ def calculate_matches(descriptor_1, descriptor_2):
     return good_matches
 
 
-def image_resize(image):
+def image_resize(image: np.ndarray, maxD: int = 256) -> np.ndarray:
     """
-    Resize a grayscale image while maintaining its aspect ratio, with the maximum dimension set to 256 pixels.
+    Resize an image while maintaining its aspect ratio, with the maximum dimension set to maxD pixels.
 
     Parameters:
-    image (numpy.ndarray): The input grayscale image to be resized. This should be a 2D numpy array with shape (height, width).
+    image (np.ndarray): The input image to be resized. This can be a 2D grayscale or a 3D color image.
+    maxD (int): The maximum dimension (width or height) to resize the image to. Default is 256 pixels.
 
     Returns:
-    numpy.ndarray: The resized image with its aspect ratio preserved, and the maximum dimension set to 256 pixels.
+    np.ndarray: The resized image with its aspect ratio preserved.
     """
-    maxD = 256
     if len(image.shape) == 2:
         height, width = image.shape
     else:
@@ -93,43 +74,20 @@ def image_resize(image):
     return image
 
 
-def find_top_images(target_path, database, num_top_images=4):
-    target_image = image_resize(cv2.imread(target_path))
-    start_time = time.perf_counter()
-    target_keypoints, target_descriptor = compute_orb(target_image)
-    top_image_ids = []
-    heapq.heapify(top_image_ids)
-
-    for image_id in database:
-        image_descriptor = database[image_id]["descriptor"]
-        image_num_keypoints = database[image_id]["n_keypoints"]
-        try:
-            current_matches = calculate_matches(target_descriptor, image_descriptor)
-            current_score = 100 * (len(current_matches) / max(len(target_keypoints), image_num_keypoints))
-        except:
-            current_score = 0
-
-        heapq.heappush(top_image_ids, (current_score, image_id))
-        if len(top_image_ids) > num_top_images:
-            heapq.heappop(top_image_ids)
-
-    result = [
-        {"top_image_id": image, "score": round(score, 3)}
-        for score, image in sorted(top_image_ids, reverse=True)
-    ]
-    search_time = time.perf_counter() - start_time
-    return result, round(search_time, 3)
-
-
-def calculate_score(keypoints_1, descriptor_1, keypoints_2, descriptor_2):
+def calculate_score(
+    keypoints_1: List[cv2.KeyPoint],
+    descriptor_1: np.ndarray,
+    keypoints_2: List[cv2.KeyPoint],
+    descriptor_2: np.ndarray,
+) -> float:
     """
     Calculate the matching score between two sets of ORB keypoints and descriptors.
 
     Parameters:
-    keypoints_1 (list): A list of keypoints from the first image. Each keypoint is an instance of cv2.KeyPoint.
-    descriptor_1 (numpy.ndarray): Descriptors corresponding to the keypoints of the first image. Each descriptor is a vector of integers.
-    keypoints_2 (list): A list of keypoints from the second image. Each keypoint is an instance of cv2.KeyPoint.
-    descriptor_2 (numpy.ndarray): Descriptors corresponding to the keypoints of the second image. Each descriptor is a vector of integers.
+    keypoints_1 (list): A list of keypoints from the first image.
+    descriptor_1 (numpy.ndarray): Descriptors corresponding to the keypoints of the first image.
+    keypoints_2 (list): A list of keypoints from the second image.
+    descriptor_2 (numpy.ndarray): Descriptors corresponding to the keypoints of the second image.
 
     Returns:
     float: A matching score between 0 and 100, rounded to three decimal places.
